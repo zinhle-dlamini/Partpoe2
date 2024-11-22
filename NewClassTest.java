8a21ee1e1be36458277b6385915c2ca7a03f7165
zinhle-dlamini <ST10445739@rcconnect.edu.za>
2024-11-22 08:00:51 +0200

    Add files via upload

---
+++ NewClassTest.java
@@ -0,0 +1,65 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.mycompany.st10445739;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author Zinhle
+ */
+public class NewClassTest {
+    
+    public NewClassTest() {
+    }
+
+    @org.junit.jupiter.api.BeforeAll
+    public static void setUpClass() throws Exception {
+    }
+
+    /**
+     *
+     * @throws Exception
+     */
+    @org.junit.jupiter.api.AfterAll
+    public static void tearDownClass() throws Exception {
+    }
+
+    @org.junit.jupiter.api.BeforeEach
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    public void tearDown() throws Exception {
+    }
+    
+    @BeforeAll
+    
+    
+    @AfterAll
+   
+    
+    @BeforeEach
+   
+    @AfterEach
+    
+
+    /**
+     * Test of main method, of class NewClass.
+     */
+    @org.junit.jupiter.api.Test
+    public void testMain() {
+        System.out.println("main");
+        String[] args = null;
+        NewClass.main(args);
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
